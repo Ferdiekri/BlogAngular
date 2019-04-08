fba7f7d9fd8615efde8d4edfbc289cd89458a150
hobetuz <hobetuz@ipartekaula.com>
2019-04-08 19:03:07 +0200

    Cambio del título y su estilo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { PhotosComponent } from './photos/photos.component';
+import { TodosComponent } from './todos/todos.component';
+import { PostsComponent } from './posts/posts.component';
+import { CommentsComponent } from './comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    AlbumsComponent,
+    PhotosComponent,
+    TodosComponent,
+    PostsComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule
