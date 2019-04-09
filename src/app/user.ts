fdfac790d7ff9bcbdd171a0c82cf8c568eb9cc4c
hobetuz <hobetuz@ipartekaula.com>
2019-04-09 18:49:58 +0200

    arreglada la estructura de users.

--- src/app/user.ts
+++ src/app/user.ts
@@ -3,14 +3,27 @@ export class User {
   name: string;
   username: string;
   email: string;
-  address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
-    geo: {
-      lat: number;
-      lng: number;
-    }
-  }
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export class Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export class Geo {
+  lat: number;
+  lng: number;
+}
+
+export class Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
